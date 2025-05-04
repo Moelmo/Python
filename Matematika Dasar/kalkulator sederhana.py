1d8827d2fb5bbb73a24af1850e9bcac3313d087f
Moelmo <98957335+Moelmo@users.noreply.github.com>
2025-05-04 16:07:37 +0700

    Update

--- Matematika Dasar/kalkulator sederhana.py
+++ Matematika Dasar/kalkulator sederhana.py
@@ -12,11 +12,31 @@ print("3. Perkalian")
 print("4. Pembagian")
 print("5. Keluar")
 print("==========================\n")
-kalkulasi = int(input("Masukkan pilihan (1/2/3/4/5): "))
-if kalkulasi == 1:
-    tambah1 = int(input("Masukkan angka pertama: "))
-    tambah2 = int(input("Masukkan angka kedua: "))
-    hasil = tambah1 + tambah2
-    print(f"Hasil penjumlahan {tambah1} + {tambah2} = {hasil}")
-else:
-    print("Operasi tidak valid")
+while True:
+    kalkulasi = int(input("Masukkan pilihan (1/2/3/4/5): "))
+    if kalkulasi == 1:
+        tambah1 = int(input("Masukkan angka pertama: "))
+        tambah2 = int(input("Masukkan angka kedua: "))
+        hasil = tambah1 + tambah2
+        print(f"Hasil penjumlahan {tambah1} + {tambah2} = {hasil}")
+    elif kalkulasi == 2:
+        kurang1 = int(input("Masukkan angka pertama: "))
+        kurang2 = int(input("Masukkan angka kedua: "))
+        hasil == kurang1 - kurang2
+        print(f"Hasil pengurangan {kurang1} - {kurang2} = {hasil}")
+    elif kalkulasi == 3:
+        kali1 = int(input("Masukkan angka pertama: "))
+        kali2 = int(input("Masukkan angka kedua: "))
+        hasil = kali1 * kali2
+        print(f"Hasil perkalian {kali1} * {kali2} = {hasil}")
+    elif kalkulasi == 4:
+        bagi1 = int(input("Masukkan angka pertama: "))
+        bagi2 = int(input("Masukkan angka kedua: "))
+        if bagi2 == 0:
+            print("Pembagian dengan nol tidak diperbolehkan")
+        else:
+            hasil = bagi1 / bagi2
+            print(f"Hasil pembagian {bagi1} / {bagi2} = {hasil}")
+
+    else:
+        print("Masukkan input yang benar")
