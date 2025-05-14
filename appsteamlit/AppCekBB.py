4279c8e8d990f66fe4ef64537167ef723d8bf422
Moelmo <moelmo165@gmail.com>
2025-05-14 20:37:56 +0700

    Update Rumus Broca Dan Hasil

--- appsteamlit/AppCekBB.py
+++ appsteamlit/AppCekBB.py
@@ -49,8 +49,31 @@ st.markdown("""---""")
 
 with st.expander("Cara penggunaan"):
     st.write("""Rumus BIM (Body Mass Index)
-BIM mengukur proporsi berat badan dengan tinggi badan.\nRumus Broca (Ideal Body Weight)
+BIM mengukur proporsi berat badan dengan tinggi badan.\n
+Rumus Broca (Ideal Body Weight)
 Broca menghitung berat badan ideal berdasarkan tinggi""")
+
+with st.expander("Broca"):
+    st.write("""
+
+✅ Cara Menggunakan Rumus Broca
+Ukur tinggi badan dalam cm.
+Contoh: 160 cm
+
+Gunakan rumus sesuai jenis kelamin:
+
+Pria:
+(Tinggi - 100) - 10% dari hasil
+= (160 - 100) - 10% = 60 - 6 = 54 kg
+
+Wanita:
+(Tinggi - 100) - 15% dari hasil
+= (160 - 100) - 15% = 60 - 9 = 51 kg
+
+Hasil = berat badan ideal.
+""")
+    
+
 with st.expander("BIM"):
     st.write("""
 
@@ -76,25 +99,76 @@ Lihat kategori berat badan:
 ≥ 30 = Obesitas
 
 """)
-with st.expander("Broca"):
-    st.write("""
 
-✅ Cara Menggunakan Rumus Broca
-Ukur tinggi badan dalam cm.
-Contoh: 160 cm
-
-Gunakan rumus sesuai jenis kelamin:
-
-Pria:
-(Tinggi - 100) - 10% dari hasil
-= (160 - 100) - 10% = 60 - 6 = 54 kg
-
-Wanita:
-(Tinggi - 100) - 15% dari hasil
-= (160 - 100) - 15% = 60 - 9 = 51 kg
-
-Hasil = berat badan ideal.
-""")
-    
 st.markdown("""---""")
-    
+
+st.markdown("<h3 style='text-align: center;'>Broca (Ideal Body Weight)</h3>", unsafe_allow_html=True)
+
+
+st.markdown(
+    """
+    <style>
+    .stRadio > div {
+        display: flex;
+        justify-content: center;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+st.markdown("<h4 style='text-align: center;'>Masukkan Jenis Kelamin anda</h4>", unsafe_allow_html=True)
+
+gender = st.radio("",
+    ["Laki - Laki", "Perempuan"],
+    horizontal=True
+)
+
+Pria = 0.10
+Wanita = 0.15
+
+if gender == "Laki - Laki":
+    Ngender = 0.10
+else:
+    Ngender = 0.15
+
+st.markdown("<h4 style='text-align: center;'>Masukkan tinggi badan anda</h4>", unsafe_allow_html=True)
+Tinggi_Broca = st.number_input("")
+Tinggi_Broca = int(Tinggi_Broca)
+
+if st.button("Cek", icon="✅"):
+    if Tinggi_Broca <= 150:
+        st.warning("Masukkan Tinggi Badan Anda Minimal 150 Ke Atas")
+    else:
+        Hasil = (Tinggi_Broca - 100) - ((Tinggi_Broca - 100) * Ngender)
+        st.markdown("<h5 style='text-align: center;'>Hasil</h5>", unsafe_allow_html=True)
+        st.write(f"""
+                Jenis Kelamin : {gender}\n
+                Tinggi badan : {Tinggi_Broca}\n
+                Hasil : {Hasil} Kg""")
+        st.success(f"Berat badan idealmu sekitar {Hasil} Kg")
+
+
+
+
+# COPYRIGHT
+
+st.markdown("---")
+st.markdown("""
+    <div style="text-align: center; font-size: 25px; color: #555;">
+        <p>Follow us on:</p>
+        <a href="https://github.com" target="https://github.com/Moelmo" style="text-decoration: none;">
+            <img src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" width="35" height="35" alt="GitHub" />
+        </a>
+        <a href="https://discord.com" target="https://discord.users/" style="text-decoration: none;">
+            <img src="https://cdn-icons-png.flaticon.com/128/2111/2111370.png" width="35" height="35" alt="Discord" />
+        </a>
+        <a href="https://www.tiktok.com" target="https://tiktok.com/@moelmo57" style="text-decoration: none;">
+            <img src="https://cdn-icons-png.flaticon.com/128/4782/4782345.png" width="35" height="35" alt="TikTok" />
+        </a>
+        <a href="https://www.instagram.com" target="https://instagram.com/moelmo.57" style="text-decoration: none;">
+            <img src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" width="35" height="35" alt="Instagram" />
+        </a>
+    </div>
+    """, unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 25px; color: #888;'>© Moelmo 2025</p>", unsafe_allow_html=True)
