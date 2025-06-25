17634f42494d37bc730674aaf2d69819710813f4
Moelmo <moelmo165@gmail.com>
2025-06-25 18:21:16 +0700

    save

--- Basic Python/auth.py
+++ Basic Python/auth.py
@@ -1,15 +1,21 @@
-data = {
-    "User": "Usr",
-    "Pw": "Pws"
-}
+# data = {
+#     "User": "Usr",
+#     "Pw": "Pws"
+# }
 
-username = input("Masukkan nama anda: ")
-password = input("Masukkan password anda: ")
+# username = input("Masukkan nama anda: ")
+# password = input("Masukkan password anda: ")
 
-# Simpan input ke data
-data["User"] = username
-data["Pw"] = password
+# # Simpan input ke data
+# data["User"] = username
+# data["Pw"] = password
 
-# Cetak hasil
-for i, (key, value) in enumerate(data.items(), start=1):
-    print(f"{i}. {key} : {value}")
+# # Cetak hasil
+# for i, (key, value) in enumerate(data.items(), start=1):
+#     print(f"{i}. {key} : {value}")
+
+
+# login sistem
+
+def mainmenu():
+    print("")
