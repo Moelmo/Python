72ff0cf9f253abf334da73a54cf3b8b357b5ccb3
Moelmo <moelmo165@gmail.com>
2025-07-23 17:28:40 +0700

    update

--- socket/6st/client.py
+++ socket/6st/client.py
@@ -1,6 +1,32 @@
 import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 12345))
-print("terhubung")
-s.close()
+def recive_msg(client_socket):
+    """Menerima pesan dari server"""
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode('utf-8')
+            print(msg)
+        except:
+            print("[ERROR] Disconnected from server.")
+            client_socket.close()
+            break
+
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 5555))
+
+    # thread untuk menerima pesan
+    recive_thread = threading.Thread(target=recive_msg, args=(client,))
+    recive_thread.start()
+
+    while True:
+        msg = input("")
+        if msg.lower() == "q":
+            client.close()
+            break
+        client.send(msg.encode('utf-8'))
+
+if __name__ == "__main__":
+    print("[CLIENT] Connection to server")
+    start_client()
--- socket/6st/server.py
+++ socket/6st/server.py
@@ -1,7 +1,55 @@
-import socket
+import socket 
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 12345))
-s.listen(1)
-conn, addr = s.accept()
-conn.close()
+# data user
+clients = []
+
+def handle_client(client_socket, address):
+    """Menangani komunikasi dengan satu client"""
+    print(f"[NEW CONNECTION] {address} connected.")
+
+    while True:
+        try:
+            # menerima pesan dari client
+            msg = client_socket.recv(1024).decode('utf-8')
+            if not msg:
+                break
+
+            print(f"[{address} : {msg}]")
+
+            broadcast(msg, client_socket)
+        except:
+            # jika terjadi error, hapus client dari list
+            print(f"[DISCONNECTED] {address} disconnected.")
+            clients.remove(client_socket)
+            client_socket.close()
+            break
+
+def broadcast(msg, sender_socket):
+    """Mengirim pesan ke semua client kecuali pengirim"""
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.send(msg.decode('utf-8'))
+            except:
+                #jika gagal mengirim, hapus cient yang bermasalah
+                clients.remove(client)
+                client.close()
+
+def start_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', 5555))
+    s.listen()
+    print("[SERVER STARTED] Waiting for connections...")
+
+    while True:
+        client_socket, address = s.accept()
+        clients.append(client_socket)
+
+        thread = threading.Thread(target=handle_client, args=(client_socket, address))
+
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+if __name__ == "__main__":
+    start_server()
